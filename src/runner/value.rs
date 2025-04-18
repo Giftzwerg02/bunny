5bef9601265fc0dc8a67c8e92cc6e85951260a19
Nick Fischer <nick.korotkin@outlook.com>
2025-04-18 12:04:06 +0200

    Re-did Value enum for lazy evaluation, added immutable strings and a color library

--- src/runner/value.rs
+++ src/runner/value.rs
@@ -1,63 +1,85 @@
+use std::cell::LazyCell;
+
 use im::HashMap;
 use im::Vector;
-use crate::ast::expressions::Color;
-use crate::types::typ::Type;
+use imstr::ImString;
+use palette::Srgba;
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Value {
+pub enum Lazy<'a> {
+    Int(LazyCell<i64, Box<dyn FnOnce() -> i64>>),
+    
+    Float(LazyCell<f64, Box<dyn FnOnce() -> f64>>),
+    
+    String(LazyCell<ImString, Box<dyn FnOnce() -> ImString>>),
+    
+    // palette does not seem to have a "general" color type
+    // so we just store it as a linear RGBA color for now
+    Color(LazyCell<Srgba, Box<dyn FnOnce() -> Srgba>>),
+    
+    // TODO Implement this
+    //Opaque(LazyCell<()>),
+    
+    // DO NOT replace LazyCell with LazyLock for it to be thread safe
+    // instead, use the thread safe version of the im crate
+    Array(
+        LazyCell<
+            Vector<Lazy<'a>>,
+            Box<dyn FnOnce() -> Vector<Lazy<'a>> + 'a>
+        >
+    ),
+
+    // Keys are eagerly evaluated, but values are lazy
+    // TODO Maybe restrict to only "reasoably" hashable keys? int, string, color?
+    Dict(
+        LazyCell<
+            HashMap<Value<'a>, Lazy<'a>>,
+            Box<dyn FnOnce() -> HashMap<Value<'a>, Lazy<'a>> + 'a>
+        >
+    ) 
+}
+
+impl<'a> Lazy<'a> {
+    pub fn new_int(value: i64) -> Self {
+        let callback: Box<dyn FnOnce() -> i64> = Box::new(move || value);
+        Lazy::Int(LazyCell::new(callback))
+    }
+
+    pub fn new_float(value: f64) -> Self {
+        let callback: Box<dyn FnOnce() -> f64> = Box::new(move || value);
+        Lazy::Float(LazyCell::new(callback))
+    }
+
+    pub fn new_string(value: ImString) -> Self {
+        let callback: Box<dyn FnOnce() -> ImString> = Box::new(move || value);
+        Lazy::String(LazyCell::new(callback))
+    }
+
+    pub fn new_color(value: Srgba) -> Self {
+        let callback: Box<dyn FnOnce() -> Srgba> = Box::new(move || value);
+        Lazy::Color(LazyCell::new(callback))
+    }
+
+    pub fn new_array(value: Vector<Lazy<'a>>) -> Self {
+        let callback: Box<dyn FnOnce() -> Vector<Lazy<'a>> + 'a> = Box::new(move || value);
+        Lazy::Array(LazyCell::new(callback))
+    }
+
+    pub fn new_dict(value: HashMap<Value<'a>, Lazy<'a>>) -> Self {
+        let callback: Box<dyn FnOnce() -> HashMap<Value<'a>, Lazy<'a>> + 'a> = Box::new(move || value);
+        Lazy::Dict(LazyCell::new(callback))
+    }
+}
+
+pub enum Value<'a> {
     Int(i64),
+    
     Float(f64),
+    
     String(String),
-    Color(ColorValue),
-    Opaque,
+    
+    Color(Srgba),
+    
+    Array(Vector<Value<'a>>),
 
-    IntArray(Vector<i64>),
-    FloatArray(Vector<f64>),
-    StringArray(Vector<String>),
-    ColorArray(Vector<ColorValue>),
-    OpaqueArray,
-
-    IntDict(HashMap<String, i64>),
-    FloatDict(HashMap<String, f64>),
-    StringDict(HashMap<String, String>),
-    ColorDict(HashMap<String, Color>),
-    OpaqueDict,
-}
-
-
-impl Value {
-    pub fn to_type(&self) -> Type {
-        match self {
-            Value::Int(_) => Type::Int,
-            Value::Float(_) => Type::Float,
-            Value::String(_) => Type::String,
-            Value::Color(_) => Type::Color,
-            Value::Opaque => Type::Opaque,
-
-            Value::IntArray(_) => Type::IntArray,
-            Value::FloatArray(_) => Type::FloatArray,
-            Value::StringArray(_) => Type::StringArray,
-            Value::ColorArray(_) => Type::ColorArray,
-            Value::OpaqueArray => Type::OpaqueArray,
-
-            Value::IntDict(_) => Type::IntDict,
-            Value::FloatDict(_) => Type::FloatDict,
-            Value::StringDict(_) => Type::StringDict,
-            Value::ColorDict(_) => Type::ColorDict,
-            Value::OpaqueDict => Type::OpaqueDict,
-        }
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct ColorValue {
-    r: u8,
-    g: u8,
-    b: u8,
-}
-
-impl ColorValue {
-    pub fn new(r: u8, g: u8, b: u8) -> ColorValue {
-        ColorValue { r, g, b }
-    }
+    Dict(HashMap<Value<'a>, Value<'a>>)
 }
